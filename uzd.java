b7d2a5927e0a4266bfa3da345324cc14db4110e6
alexPython23 <chernolikhkarina@gmail.com>
2025-11-26 15:22:52 +0200

    uzd1

--- uzd.java
+++ uzd.java
@@ -1,8 +1,21 @@
 public class chap44{
     public static void main(String[] args) {
-      
+      Scanner scanner = new Scanner(System.in);
     }
-  public static void uzd1(){}
+  public static void uzd1(){
+    System.out.print("Enter your name: ");
+        String name = in.nextLine().trim();
+        int spaceIndex = name.indexOf(" ");
+        if (spaceIndex > 0) {  
+            String firstName = name.substring(0, spaceIndex);
+            String lastName = name.substring(spaceIndex + 1);
+            lastName = lastName.toUpperCase();
+            String result = firstName + " " + lastName;
+            System.out.println(result);
+        } else {
+            System.out.println(name);
+        }
+    }
   public static void uzd2(){}
   public static void uzd3(){}
   public static void uzd4(){}
