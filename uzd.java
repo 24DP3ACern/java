90fe2f5513d440d1d302a48104d673b3a1563cab
alexPython23 <chernolikhkarina@gmail.com>
2025-11-26 15:13:53 +0200

    newuzds

---
+++ uzd.java
@@ -0,0 +1,12 @@
+public class chap44{
+    public static void main(String[] args) {
+      
+    }
+  public static void uzd1(){}
+  public static void uzd2(){}
+  public static void uzd3(){}
+  public static void uzd4(){}
+  public static void uzd5(){}
+  public static void uzd6(){}
+  public static void uzd7(){}
+}
